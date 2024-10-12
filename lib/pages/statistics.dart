f5a5f091ff4aa704609f9f8cf7fe7af2bf5857d4
jrolana <olanajhoanna@gmail.com>
2024-10-12 15:33:58 +0800

    [Feature][Olana] AppBar, TabBar, and  AppTheme

---
+++ lib/pages/statistics.dart
@@ -0,0 +1,80 @@
+import 'package:buttons_tabbar/buttons_tabbar.dart';
+import 'package:flutter/material.dart';
+import '../utils/styles.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:iconly/iconly.dart';
+
+class Statistics extends StatefulWidget {
+  Statistics({super.key});
+
+  @override
+  State<Statistics> createState() => _StatisticsState();
+}
+
+class _StatisticsState extends State<Statistics> with TickerProviderStateMixin {
+  static const List<Tab> tabs = [
+    Tab(
+      text: 'Day',
+    ),
+    Tab(
+      text: 'Week',
+    ),
+    Tab(
+      text: 'Year',
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            titleTextStyle: TextStyle(
+                color: StyleColor.primaryText,
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+                fontFamily: GoogleFonts.lexendDeca().fontFamily),
+            title: const Text(
+              "Routine Statistics",
+            ),
+            leading: IconButton(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              icon: const Icon(IconlyBold.arrow_left),
+              onPressed: () {
+                print("Pressed back");
+              },
+            ),
+          ),
+          body: Column(
+            children: <Widget>[
+              ButtonsTabBar(
+                radius: 14,
+                width: 125,
+                height: 60,
+                buttonMargin:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                contentCenter: true,
+                backgroundColor: StyleColor.primary,
+                unselectedBackgroundColor: StyleColor.alternate,
+                labelStyle: StyleText.labelStyle,
+                unselectedLabelStyle: StyleText.unselectedLabelStyle,
+                tabs: tabs,
+              ),
+              // To be replaced with pages
+              Expanded(
+                child: TabBarView(children: [
+                  Icon(Icons.directions_boat),
+                  Icon(Icons.directions_boat),
+                  Icon(Icons.directions_boat),
+                ]),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
