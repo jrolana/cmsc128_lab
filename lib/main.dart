43ef7d02773ffb7c45d6dc9f5b274ee0885ebb8e
Dom <domdicar@gmail.com>
2024-10-16 19:49:26 +0800

    [Dicar} [NavBar Feature]
    Connected Bottom Navbar with Statistics Page and HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cmsc128_lab/pages/navbar.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 // import 'package:cmsc128_lab/pages/home.dart';
@@ -29,7 +30,7 @@ class MyApp extends StatelessWidget {
                   fontWeight: FontWeight.w600,
                   fontFamily: GoogleFonts.lexendDeca().fontFamily),
             )),
-        home: Statistics(),
+        home: BottomNavBar(),
       ),
     );
   }
