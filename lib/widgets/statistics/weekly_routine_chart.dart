21b5f5eeb52a55b7eedf562907d4add87669e4e6
jrolana <olanajhoanna@gmail.com>
2024-11-14 01:03:57 +0800

    [Feature][Olana] Weekly Statistics - Column Chart

--- lib/widgets/statistics/weekly_routine_chart.dart
+++ lib/widgets/statistics/weekly_routine_chart.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:cmsc128_lab/data/statistics_data.dart';
+import 'package:cmsc128_lab/utils/styles.dart';
 
 class WeeklyRoutineChart extends StatefulWidget {
   const WeeklyRoutineChart({super.key});
@@ -18,66 +19,50 @@ class _WeeklyRoutineChartState extends State<WeeklyRoutineChart> {
       return routine.name;
     }).toList();
 
+    final List<int> indices =
+        List.generate(routineNames.length, (index) => index);
+
     return Center(
         child: Container(
+      height: 400,
       margin: const EdgeInsets.all(20),
       decoration: const BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.all(Radius.circular(20))),
       child: SfCartesianChart(
         margin: const EdgeInsets.all(30),
-        title: ChartTitle(
-            text: 'See your progress for this week.',
-            textStyle: TextStyle(
-                fontSize: 12, fontFamily: GoogleFonts.lexendDeca().fontFamily)),
         legend: Legend(
           isVisible: true,
           position: LegendPosition.bottom,
           textStyle: TextStyle(fontFamily: GoogleFonts.lexendDeca().fontFamily),
         ),
-        primaryXAxis: const CategoryAxis(),
-        series: <CartesianSeries>[
-          StackedColumnSeries<WeekRoutine, String>(
-            name: routineNames[0],
+        primaryXAxis: CategoryAxis(
+          labelStyle: TextStyle(
+            color: Colors.black54,
+            fontSize: 12,
+            fontWeight: FontWeight.w600,
+            fontFamily: GoogleFonts.lexendDeca().fontFamily,
+          ),
+        ),
+        primaryYAxis: NumericAxis(
+          maximum: 100,
+          labelStyle: TextStyle(
+            color: Colors.black54,
+            fontFamily: GoogleFonts.lexendDeca().fontFamily,
+          ),
+        ),
+        series: indices.map((i) {
+          return StackedColumnSeries<WeekRoutine, String>(
+            name: routineNames[i],
             dataSource: chartData,
             xValueMapper: (WeekRoutine data, _) => data.day,
             yValueMapper: (WeekRoutine data, _) =>
-                data.weeklyCompletionRates[0],
-            pointColorMapper: (WeekRoutine data, _) => data.colors[0],
-          ),
-          StackedColumnSeries<WeekRoutine, String>(
-            name: routineNames[1],
-            dataSource: chartData,
-            xValueMapper: (WeekRoutine data, _) => data.day,
-            yValueMapper: (WeekRoutine data, _) =>
-                data.weeklyCompletionRates[1],
-            pointColorMapper: (WeekRoutine data, _) => data.colors[1],
-          ),
-          StackedColumnSeries<WeekRoutine, String>(
-            name: routineNames[2],
-            dataSource: chartData,
-            xValueMapper: (WeekRoutine data, _) => data.day,
-            yValueMapper: (WeekRoutine data, _) =>
-                data.weeklyCompletionRates[2],
-            pointColorMapper: (WeekRoutine data, _) => data.colors[2],
-          ),
-          StackedColumnSeries<WeekRoutine, String>(
-            name: routineNames[3],
-            dataSource: chartData,
-            xValueMapper: (WeekRoutine data, _) => data.day,
-            yValueMapper: (WeekRoutine data, _) =>
-                data.weeklyCompletionRates[3],
-            pointColorMapper: (WeekRoutine data, _) => data.colors[3],
-          ),
-          StackedColumnSeries<WeekRoutine, String>(
-            name: routineNames[4],
-            dataSource: chartData,
-            xValueMapper: (WeekRoutine data, _) => data.day,
-            yValueMapper: (WeekRoutine data, _) =>
-                data.weeklyCompletionRates[4],
-            pointColorMapper: (WeekRoutine data, _) => data.colors[4],
-          ),
-        ],
+                data.weeklyCompletionRates[i],
+            pointColorMapper: (WeekRoutine data, _) => data.colors[i],
+            width: 0.2,
+            borderRadius: const BorderRadius.all(Radius.circular(15)),
+          );
+        }).toList(),
       ),
     ));
   }
