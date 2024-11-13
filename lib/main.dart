3831eda059589b2220d95ad0cb7e24a25d9dcee7
jules <jcmontinola@up.edu.ph>
2024-11-13 19:50:18 +0800

    [Fix][Montinola] Merge Main

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,7 @@ class MyApp extends StatelessWidget {
                   fontWeight: FontWeight.w600,
                   fontFamily: GoogleFonts.lexendDeca().fontFamily),
             )),
-        home: BottomNavBar(),
+        home: WelcomeScreen(),
       ),
     );
   }
