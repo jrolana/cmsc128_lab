931f26cb68c4888977f926ade24c6dfdb5b71399
redrogue010 <piageaguirre@gmail.com>
2024-11-28 14:05:05 +0800

    Created routine list file

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
                 fontFamily: GoogleFonts.lexendDeca().fontFamily),
           ),
         ),
-        home: RoutineSession(),
+        home: RoutineSession('243411411'),
       ),
     );
   }
