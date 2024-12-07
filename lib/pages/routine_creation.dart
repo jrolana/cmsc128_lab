b37082a2879c61401c8de4309ebb195e20c47a68
redrogue010 <piageaguirre@gmail.com>
2024-12-07 16:10:15 +0800

    Add Task Block Button

--- lib/pages/routine_creation.dart
+++ lib/pages/routine_creation.dart
@@ -3,6 +3,7 @@ import 'package:cmsc128_lab/widgets/routineWidgets/routine_creation_activity_blo
 import 'package:cmsc128_lab/utils/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+
 class RoutineCreation extends StatefulWidget {
   const RoutineCreation({super.key});
 
@@ -12,36 +13,37 @@ class RoutineCreation extends StatefulWidget {
   }
 }
 
-class _RoutineCreationDefaultState extends State<RoutineCreation> with TickerProviderStateMixin {
-  int actCount  = 1;
-  List<ActivityBlock> activityBlocks = [ActivityBlock()];
+class _RoutineCreationDefaultState extends State<RoutineCreation>
+    with TickerProviderStateMixin {
+  int actCount = 1;
+  List activityBlocks = [ActivityBlock()];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         floatingActionButton:
             Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-          TextButton(
-            style: TextButton.styleFrom(backgroundColor: StyleColor.primary),
+          IconButton(
+              onPressed: () {
+                addActivity();
+              },
+              style: TextButton.styleFrom(backgroundColor: StyleColor.primary),
+              icon: Icon(
+                Icons.playlist_add_rounded,
+                color: Colors.white,
+              )),
+          ElevatedButton(
+              onPressed: () {
+                print('Add Task Block');
+              },
+              child: Text('Add Task Block')),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(backgroundColor: StyleColor.primary),
             onPressed: () {},
-            child: Container(
-                padding: EdgeInsets.all(8),
-                color: StyleColor.primary,
-                child: Text("Create Routine",
-                    style: TextStyle(color: Colors.white))),
-          ),
-          Container(
-              padding: EdgeInsets.all(9),
-              child: TextButton(
-                  onPressed: () {
-                    addActivity();
-                  },
-                  style:
-                      TextButton.styleFrom(backgroundColor: StyleColor.primary),
-                  child: Icon(
-                    Icons.playlist_add_rounded,
-                    color: Colors.white,
-                  ))),
+            child:Text("Create Routine",
+                    style: TextStyle(color: Colors.white),
+          )),
         ]),
         appBar: AppBar(
           leading: IconButton(
@@ -54,77 +56,75 @@ class _RoutineCreationDefaultState extends State<RoutineCreation> with TickerPro
           title: const Text('Create a Routine'),
         ),
         body: Column(
-              children: [
-                RCreationActivityName(),
-                SizedBox(height: 20),
-                Expanded(
-                    child: ListView.separated(
-                        itemBuilder: (context, index){
-                          return ListTile(
-
-                            title: activityBlocks[index],
-                            trailing: IconButton(onPressed: (){
+          children: [
+            RCreationActivityName(),
+            SizedBox(height: 20),
+            Expanded(
+                child: ListView.separated(
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: activityBlocks[index],
+                        trailing: IconButton(
+                            onPressed: () {
                               setState(() {
                                 activityBlocks.removeAt(index);
-                                actCount-=1;
+                                actCount -= 1;
                               });
-                              }, icon: Icon(Icons.delete)),
-                          );
-                        },
-                        separatorBuilder: (context, index){
-                          return const SizedBox(
-                            height: 10,
-                          );
-                        },
-                        itemCount: actCount
-                    )
-                )
-              ],
-          ),
+                            },
+                            icon: Icon(Icons.delete)),
+                      );
+                    },
+                    separatorBuilder: (context, index) {
+                      return const SizedBox(
+                        height: 10,
+                      );
+                    },
+                    itemCount: actCount))
+          ],
+        ),
       ),
     );
   }
 
-  Widget makeList(){
+  Widget makeList() {
     return ListView(
-      children: [
-        ActivityBlock()
-      ],
+      children: [ActivityBlock()],
     );
   }
-  void addActivity(){
+
+  void addActivity() {
     setState(() {
       activityBlocks.add(ActivityBlock());
-      actCount+=1;
+      actCount += 1;
     });
   }
 
-  // TODO figure out reorderable list view if possible
-  // Widget makeList() {
-  //   return ReorderableListView(
-  //     proxyDecorator: (child,index,animation) =>
-  //         Material(
-  //             borderRadius: BorderRadius.circular(100),
-  //             child:child
-  //         ),
-  //     shrinkWrap: true,
-  //     children: [
-  //       for (int index = 0; index < actCount; index += 1)
-  //         Padding(
-  //           padding: EdgeInsets.symmetric(vertical: 2.0),
-  //           key: Key('$index'),
-  //           child:ActivityBlock(),
-  //         )
-  //     ],
-  //     onReorder: (int oldIndex, int newIndex) {
-  //       setState(() {
-  //         if (oldIndex < newIndex) {
-  //           newIndex -= 1;
-  //         }
-  //         final int item = _items.removeAt(oldIndex);
-  //         _items.insert(newIndex, item);
-  //       });
-  //     },
-  //   );
-  // }
+// TODO figure out reorderable list view if possible
+// Widget makeList() {
+//   return ReorderableListView(
+//     proxyDecorator: (child,index,animation) =>
+//         Material(
+//             borderRadius: BorderRadius.circular(100),
+//             child:child
+//         ),
+//     shrinkWrap: true,
+//     children: [
+//       for (int index = 0; index < actCount; index += 1)
+//         Padding(
+//           padding: EdgeInsets.symmetric(vertical: 2.0),
+//           key: Key('$index'),
+//           child:ActivityBlock(),
+//         )
+//     ],
+//     onReorder: (int oldIndex, int newIndex) {
+//       setState(() {
+//         if (oldIndex < newIndex) {
+//           newIndex -= 1;
+//         }
+//         final int item = _items.removeAt(oldIndex);
+//         _items.insert(newIndex, item);
+//       });
+//     },
+//   );
+// }
 }
