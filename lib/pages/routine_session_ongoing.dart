1b72dbd15b3b19aa29ee835e1dc7023ef32aa872
redrogue010 <piageaguirre@gmail.com>
2024-12-14 00:30:10 +0800

    Fixed session skipping

--- lib/pages/routine_session_ongoing.dart
+++ lib/pages/routine_session_ongoing.dart
@@ -9,9 +9,10 @@ import '../utils/firestore_utils.dart';
 class RoutineSessionOngoing extends StatefulWidget {
   final String routineID;
   int actNum;
-  final Map<String,dynamic> taskIDs;
-  RoutineSessionOngoing(this.routineID, this.actNum, this.taskIDs,{super.key});
-  int skipped = 0;
+  final Map<String, dynamic> taskIDs;
+
+  RoutineSessionOngoing(this.routineID, this.actNum, this.taskIDs, {super.key});
+
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
@@ -22,25 +23,38 @@ class RoutineSessionOngoing extends StatefulWidget {
 class _StateRoutineSessionOngoing extends State<RoutineSessionOngoing> {
   FirestoreUtils db = FirestoreUtils();
   final PageController _pageViewController = PageController();
+  List skip = [];
+
   @override
   void initState() {
     // TODO: implement initState
     getNumAct();
+
     super.initState();
   }
+
   void getNumAct() async {
-    await db.getActivities(widget.routineID).get().then((snap){
+    await db
+        .getActivities(widget.routineID)
+        .orderBy("order", descending: false)
+        .get()
+        .then((snap) {
       var acts = snap.docs;
-      for(var x in acts){
+      int index = 0;
+      for (var x in acts) {
         Activity data = x.data() as Activity;
-        if(data.type == "taskblock"){
-          if(!widget.taskIDs.containsKey(data.category)){
-            widget.actNum -=1;
+        if (data.type == "taskblock") {
+          if (!widget.taskIDs.containsKey(data.category)) {
+            widget.actNum -= 1;
+            skip.add(index);
           }
         }
+        index += 1;
       }
+      print(skip);
     });
   }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -56,33 +70,34 @@ class _StateRoutineSessionOngoing extends State<RoutineSessionOngoing> {
         title: const Text('Routine Session'),
       ),
       body: StreamBuilder(
-          stream: db.getActivities(widget.routineID).orderBy("order",descending: false).snapshots(),
-          builder: (context, snapshot){
+          stream: db
+              .getActivities(widget.routineID)
+              .orderBy("order", descending: false)
+              .snapshots(),
+          builder: (context, snapshot) {
             List activities = snapshot.data?.docs ?? [];
-            if(activities.isNotEmpty){
-                return PageView.builder(
-                  itemBuilder: (context, index) {
-                    Activity act = activities[index].data();
-                    if (act.type == "activity") {
-                      return RoutineSessionTimer(
-                          act.name, act.duration,
-                          act.icon, index, _navigatePage);
-                    }
-                    if(widget.taskIDs.isNotEmpty){
-                    return RoutineSessionTimerTasks(
-                        act.category, act.duration, index, _navigatePage,
-                        widget.taskIDs[act.category]);}
-                    }
-                  ,
-                  controller: _pageViewController,
-                  onPageChanged: _handlePageChange,
-                  physics: const NeverScrollableScrollPhysics(),
-                );
-              }else{
+            if (activities.isNotEmpty) {
+              return PageView.builder(
+                itemBuilder: (context, index) {
+                  Activity act = activities[index].data();
+                  if (act.type == "activity") {
+                    return RoutineSessionTimer(
+                        act.name, act.duration, act.icon, index, _navigatePage);
+                  }
+                  if (widget.taskIDs.isNotEmpty) {
+                    return RoutineSessionTimerTasks(act.category, act.duration,
+                        index, _navigatePage, widget.taskIDs[act.category]);
+                  }
+                  return SizedBox(height: 0,width: 0);
+                },
+                controller: _pageViewController,
+                onPageChanged: _handlePageChange,
+                physics: const NeverScrollableScrollPhysics(),
+              );
+            } else {
               return Text("Getting Routine Data");
             }
-          }
-          ),
+          }),
     );
   }
 
@@ -91,31 +106,33 @@ class _StateRoutineSessionOngoing extends State<RoutineSessionOngoing> {
   }
 
   void _navigatePage(int index) {
-    print( _pageViewController.page!.round());
-    print(index);
-    if(_pageViewController.page != null){
-      print("Here 1");
-      if( index > _pageViewController.page!.round()){
-        print("Here 2");
-        _pageViewController.nextPage( duration: const Duration(milliseconds: 400),
-          curve: Curves.easeOutExpo,);
-      }else{
-        print("Here 3");
-        _pageViewController.previousPage( duration: const Duration(milliseconds: 400),
-          curve: Curves.easeOutExpo,);
+    if (index >= widget.actNum) {
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => RoutineSessionComplete(widget.routineID)));
+    }else{
+
+    if (_pageViewController.page != null) {
+      if (index > _pageViewController.page!.round()) {
+        print(skip.contains(index));
+        while(skip.contains(index)) {
+          index++;
+        }
+        _pageViewController.animateToPage(index,
+            duration: const Duration(milliseconds: 400),
+            curve: Curves.easeOutExpo
+        );
+      } else {
+        while (skip.contains(index)) {
+          index--;
+        }
+        _pageViewController.animateToPage(index,
+            duration: const Duration(milliseconds: 400),
+            curve: Curves.easeOutExpo
+        );
       }
+    }
 
-    }
-    if (index == widget.actNum) {
-      Navigator.push(context, MaterialPageRoute(builder: (context)=>RoutineSessionComplete(widget.routineID)));
-    }
-    //  else {
-    //   _pageViewController.animateToPage(
-    //     index,
-    //     duration: const Duration(milliseconds: 400),
-    //     curve: Curves.easeOutExpo,
-    //   );
-    // }
-  }
+  }}
 }
-
